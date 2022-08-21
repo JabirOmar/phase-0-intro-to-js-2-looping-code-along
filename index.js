f443c87824b9e1235309c0ccf85e9b5852f128c6
Jabir Omar <jabiromar400@gmail.com>
2022-08-21 18:27:34 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards([name1, name2, name3], event) {
+     writeCards = [
+        `Thank you, ${name1}, for the wonderful ${event} gift!`,
+        `Thank you, ${name2}, for the wonderful ${event} gift!`,
+        `Thank you, ${name3}, for the wonderful ${event} gift!`,
+]
+return writeCards;
+}
+
+
+function countDown(num){
+    for (let num = 10; num >= 0; num--){
+       console.log(num)
+    }
+    return num;
+}
+countDown()
+
+
+
